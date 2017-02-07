3f10e7dc3a2f13c44ca814c83b1740c9917fe409
Wenderson Silva <wenderson.silva@transire.com.br>
2017-02-07 08:30:42 -0400

    trabalhando com classes helper

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,24 +6,38 @@ class NegociacaoController{
 		this._inputQuantidade = document.querySelector('#quantidade');
 		this._inputData = documet.querySelector('#data');
 		this._inputValor = document.querySelector('#valor');
+		this._listaNegociacoes = new ListaNegociacoes();
 	}
 	
 	adiciona(event) {
 		event.preventDefault();
 
-		let data = new Date(...
+		/*let data = new Date(...
 			this._inputData.value
 			    .split("-")
 			    .map((item,indice) => return item - indice % 2 )
                
-		);
+		);*/
+        let helper = new DateHelper();
+		let data = helper.textoParaData(this._inputData.value);
+		diaMesAno = helper.dataParaTexto(negociacao.data);
         
-		let negociacao = new negociacao(
+		//let negociacao =
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+		console.log(negociacao);
+	}
+
+	_criaNegociacao(){
+        return new negociacao(
            data,
            this._inputQuantidade.value,
            this._inputValor.value
 		);
+	}
 
-		console.log(negociacao);
+	_limpaFormulario(){
+		this._inputData.value = '';
+		this._inputValor.value = '';
 	}
 }
